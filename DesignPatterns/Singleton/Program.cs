ad810a7b98b77ad2dd0f82b33b93489d6357334d
Sofia Munoz <sofia.munoz@valtech.com>
2022-11-26 02:57:10 -0300

    first commit

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        public sealed class Singleton
+        {   
+            private Singleton() { }
+
+            private static Singleton _instance;
+
+            public static Singleton GetInstance()
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+
+            public void someBusinessLogic()
+            {
+                // ...
+            }
+        }
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
